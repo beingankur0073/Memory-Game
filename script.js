3ea4d340fc49f73d361919fded27d45cc109a761
Ankur Mukherjee <mukherjeeankur0073@gmail.com>
2025-07-11 22:22:28 +0530

    Refactor HTML structure and enhance JavaScript functionality; added message display for game interactions and improved CSS for responsiveness.

--- script.js
+++ script.js
@@ -1,127 +1,89 @@
-const cardArray=[
-    {
-        name:'fries',
-        img:'images/fries.png'
-    },
-    {
-        name:'cheeseburger',
-        img:'images/cheeseburger.png'
-    },
-    {
-        name:'ice-cream',
-        img:'images/ice-cream.png'
-    },
-    {
-        name:'milkshake',
-        img:'images/milkshake.png'
-    },
-    {
-        name:'pizza',
-        img:'images/pizza.png'
-    },
-    {
-        name:'hotdog',
-        img:'images/hotdog.png'
-    },
+const cardArray = [
+    { name: 'fries', img: 'images/fries.png' },
+    { name: 'cheeseburger', img: 'images/cheeseburger.png' },
+    { name: 'ice-cream', img: 'images/ice-cream.png' },
+    { name: 'milkshake', img: 'images/milkshake.png' },
+    { name: 'pizza', img: 'images/pizza.png' },
+    { name: 'hotdog', img: 'images/hotdog.png' },
+    { name: 'fries', img: 'images/fries.png' },
+    { name: 'cheeseburger', img: 'images/cheeseburger.png' },
+    { name: 'ice-cream', img: 'images/ice-cream.png' },
+    { name: 'milkshake', img: 'images/milkshake.png' },
+    { name: 'pizza', img: 'images/pizza.png' },
+    { name: 'hotdog', img: 'images/hotdog.png' }
+];
 
-    {
-        name:'fries',
-        img:'images/fries.png'
-    },
-    {
-        name:'cheeseburger',
-        img:'images/cheeseburger.png'
-    },
-    {
-        name:'ice-cream',
-        img:'images/ice-cream.png'
-    },
-    {
-        name:'milkshake',
-        img:'images/milkshake.png'
-    },
-    {
-        name:'pizza',
-        img:'images/pizza.png'
-    },
-    {
-        name:'hotdog',
-        img:'images/hotdog.png'
-    },
-]
+cardArray.sort(() => 0.5 - Math.random());
 
-cardArray.sort(()=>0.5-Math.random()) // way to sort randomly
-//console.log(cardArray)
+const gridDisplay = document.querySelector('#grid');
+const resultDisplay = document.querySelector('#result');
+const messageDisplay = document.querySelector('#message'); // Add this element in your HTML
 
-const gridDisplay=document.querySelector('#grid')
-const resultDisplay=document.querySelector('#result')
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
-let cardsChosen=[] //empty array
-// let as further size is changed
-let cardsChosenIds=[]
-const cardsWon=[]
-
-function createBoard(){
-    for(let i=0;i<cardArray.length;i++){
-       const card=document.createElement('img') //see
-       card.setAttribute('src','images/blank.png') // setting src as blank.png
-       card.setAttribute('border','3px solid black')
-       
-       card.setAttribute('data-id',i)
-       card.addEventListener('click',flipCard) // call back function
-       gridDisplay.appendChild(card)
-       //console.log(card)
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.png');
+        card.setAttribute('border', '3px solid black');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
+        gridDisplay.appendChild(card);
     }
 }
 
-createBoard()
+function showMessage(msg, isSuccess = false) {
+    messageDisplay.textContent = msg;
+    messageDisplay.style.color = isSuccess ? 'green' : 'red';
+    messageDisplay.style.display = 'block';
+    setTimeout(() => {
+        messageDisplay.style.display = 'none';
+    }, 1500);
+}
 
-function checkMatch(){
-    console.log('check for a match!')
-   const cards=document.querySelectorAll('img')
-const optionOneId=cardsChosenIds[0]
-const optionTwoId=cardsChosenIds[1]
+createBoard();
 
-    console.log(cards)
-    if(optionTwoId==optionOneId){
-        alert('You clicked the same image!')
-        cards[optionOneId].setAttribute('src','images/blank.png')
+function checkMatch() {
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenIds[0];
+    const optionTwoId = cardsChosenIds[1];
+
+    if (optionTwoId == optionOneId) {
+        showMessage("You clicked the same card! Try a different one.");
+        cards[optionOneId].setAttribute('src', 'images/blank.png');
+    } else if (cardsChosen[0] == cardsChosen[1]) {
+        showMessage("Great job! You found a match!", true);
+        cards[optionOneId].setAttribute('src', 'images/white.png');
+        cards[optionTwoId].setAttribute('src', 'images/white.png');
+        cards[optionOneId].removeEventListener('click', flipCard);
+        cards[optionTwoId].removeEventListener('click', flipCard);
+        cardsWon.push(cardsChosen);
+    } else {
+        showMessage("No match this time. Keep trying!");
+        setTimeout(() => {
+            cards[optionOneId].setAttribute('src', 'images/blank.png');
+            cards[optionTwoId].setAttribute('src', 'images/blank.png');
+        }, 500);
     }
-   else if(cardsChosen[0]==cardsChosen[1]){
-        alert('You found a match!')
-        cards[optionOneId].setAttribute('src','images/white.png')
-        cards[optionTwoId].setAttribute('src','images/white.png')
 
-        cards[optionOneId].removeEventListener('click',flipCard) //removing the flipcard event that added
-        cards[optionTwoId].removeEventListener('click',flipCard)
+    resultDisplay.textContent = `Matches Found: ${cardsWon.length}`;
+    cardsChosen = [];
+    cardsChosenIds = [];
 
-        cardsWon.push(cardsChosen)
-    }
-    else{
-        cards[optionOneId].setAttribute('src','images/blank.png')
-        cards[optionTwoId].setAttribute('src','images/blank.png')
-        alert('Sorry try again!')
-    }
-
-    resultDisplay.innerText=cardsWon.length
-    cardsChosen=[]
-    cardsChosenIds=[]
-
-    if(cardsWon.length==cardArray.length/2){
-        resultDisplay.innerText='Congratulations you found them all!'
+    if (cardsWon.length == cardArray.length / 2) {
+        showMessage("Congratulations! You've found all matches!", true);
+        resultDisplay.textContent = "You won! All matches found!";
     }
 }
 
-function flipCard(){
-    // getting it's data id
-    const cardId=this.getAttribute('data-id') //In an event, this refers to the element that received the event.
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenIds.push(cardId)
-    console.log(cardsChosen)
-    console.log(cardsChosenIds)
-    console.log("clicked",cardId)
-    this.setAttribute('src',cardArray[cardId].img)
-    if(cardsChosen.length==2){
-        setTimeout(checkMatch,500)
+function flipCard() {
+    const cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500);
     }
 }
